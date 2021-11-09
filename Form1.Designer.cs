4469a8a7dff59c9102aabc68e5c9759d3ade660a
khansaimani <khansaimani1919@gmail.com>
2021-11-09 07:40:04 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace BetaMart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.Nama = new System.Windows.Forms.TextBox();
+            this.Code = new System.Windows.Forms.TextBox();
+            this.Price = new System.Windows.Forms.TextBox();
+            this.Stock = new System.Windows.Forms.TextBox();
+            this.Edit = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Search = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Show = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Code";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Stock";
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(89, 19);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(257, 20);
+            this.ID.TabIndex = 5;
+            // 
+            // Nama
+            // 
+            this.Nama.Location = new System.Drawing.Point(89, 47);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(257, 20);
+            this.Nama.TabIndex = 6;
+            // 
+            // Code
+            // 
+            this.Code.Location = new System.Drawing.Point(89, 76);
+            this.Code.Name = "Code";
+            this.Code.Size = new System.Drawing.Size(257, 20);
+            this.Code.TabIndex = 7;
+            // 
+            // Price
+            // 
+            this.Price.Location = new System.Drawing.Point(89, 105);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(257, 20);
+            this.Price.TabIndex = 8;
+            // 
+            // Stock
+            // 
+            this.Stock.Location = new System.Drawing.Point(89, 132);
+            this.Stock.Name = "Stock";
+            this.Stock.Size = new System.Drawing.Size(257, 20);
+            this.Stock.TabIndex = 9;
+            // 
+            // Edit
+            // 
+            this.Edit.Location = new System.Drawing.Point(15, 181);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(75, 23);
+            this.Edit.TabIndex = 10;
+            this.Edit.Text = "Edit";
+            this.Edit.UseVisualStyleBackColor = true;
+            this.Edit.Click += new System.EventHandler(this.Edit_Click);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(116, 181);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 11;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(213, 181);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 12;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(395, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Search";
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(472, 19);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(257, 20);
+            this.Search.TabIndex = 14;
+            this.Search.TextChanged += new System.EventHandler(this.Search_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(398, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(355, 326);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Show
+            // 
+            this.Show.Location = new System.Drawing.Point(678, 392);
+            this.Show.Name = "Show";
+            this.Show.Size = new System.Drawing.Size(75, 23);
+            this.Show.TabIndex = 16;
+            this.Show.Text = "Show";
+            this.Show.UseVisualStyleBackColor = true;
+            this.Show.Click += new System.EventHandler(this.Show_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Show);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Edit);
+            this.Controls.Add(this.Stock);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.Code);
+            this.Controls.Add(this.Nama);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.TextBox Nama;
+        private System.Windows.Forms.TextBox Code;
+        private System.Windows.Forms.TextBox Price;
+        private System.Windows.Forms.TextBox Stock;
+        private System.Windows.Forms.Button Edit;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Show;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace BetaMart
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection koneksi = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\Wigan\UKK\BetaMartDB.accdb");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Edit_Click(object sender, EventArgs e)
+        {
+            koneksi.Open();
+            OleDbCommand cmd = koneksi.CreateCommand();
+            string query = "update products set Nama='" + Nama.Text + "' ,Stock='" + Stock.Text + "' ,Price='" + Price.Text + "' ,Code='" + Code.Text + "' where ID=" + ID.Text + "";
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            koneksi.Close();
+            ID.Text = "";
+            Nama.Text = "";
+            Stock.Text = "";
+            Price.Text = "";
+            Code.Text = "";
+            MessageBox.Show("Barang kamu sudah berhasil diedit");
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            koneksi.Open();
+            string perintah = "INSERT INTO products (Nama, Code, Price, Stock) values (' " + Nama.Text + " ' , ' " + Code.Text + " ' , ' " + Price.Text + " ' , ' " + Stock.Text + " ' )";
+            OleDbCommand cmd = new OleDbCommand(perintah, koneksi);
+            cmd.ExecuteNonQuery();
+            koneksi.Close();
+            MessageBox.Show("Data Berhasil Tersimpan");
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            koneksi.Open();
+            OleDbCommand cmd = koneksi.CreateCommand();
+            string query = "delete from products where ID=" + ID.Text + "";
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            koneksi.Close();
+            ID.Text = "";
+            MessageBox.Show("Menghapus barang berhasil");
+        }
+        void ShowData()
+        {
+            koneksi.Open();
+            string query = "select * from products";
+            OleDbDataAdapter da = new OleDbDataAdapter(query, koneksi);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            koneksi.Close();
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+
+        private void Search_TextChanged(object sender, EventArgs e)
+        {
+            koneksi.Open();
+            string perintah = "select * from products where Nama='" + Search.Text + "'or Code='" + Search.Text + "'or Price='" + Search.Text + "'or Stock='" + Search.Text + "'";
+            OleDbDataAdapter da = new OleDbDataAdapter(perintah, koneksi);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            koneksi.Close();
+        }
+    }
+}
